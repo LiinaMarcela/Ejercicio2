68aebb930c9e391d431d71d053da89fac64473bc
LiinaMarcela <lmbarrera35@misena.edu.co>
2016-02-28 07:43:30 -0500

    ejercicio2
    hola mundo

---
+++ Ejercicio2/src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio2;
+
+/**
+ *
+ * @author klf
+ */
+public class Ejercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+    }
+}
